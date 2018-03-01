611d590a2a0342cc1a2f58b3ffef7434d5955fc5
Shiv Prakash Anand <shivakp2111@gmail.com>
2018-03-01 14:03:56 +0530

    01-03-2018

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var users = require('./app_server/routes/users');
 
 var app = express();
 
-// view engine setup tells the middleWare where to find the views for the app
+// View engine setup tells the middleWare where to find the views for the app
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'jade');
 
