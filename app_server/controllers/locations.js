fac5af20208116eebd61ea03855e0f1d01cf6928
Shiv Prakash Anand <shivakp2111@gmail.com>
2018-03-03 12:59:40 +0530

    locations added

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,15 +1,18 @@
 
 /** Get home page */
+// home page controller
 module.exports.homelist = function(req, res) {
-    res.render('index', {title: 'Home'});
+    res.render('locations-list', {title: 'Home'});
 };
 
 /** Get location-info page */
+// location-info page controller
 module.exports.locationInfo = function(req, res) {
     res.render('index', {title: 'Location Info'});
 };
 
 /** Get Add-Review page */
+// Add-Review page controller
 module.exports.addReview = function(req, res){
     res.render('index', {title: 'Add Review'});
 };
