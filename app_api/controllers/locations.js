9a106ac13edd72e8fe9fb859e31ce57d33719805
Shiv Prakash Anand <shivakp2111@gmail.com>
2018-03-18 00:46:04 +0530

    added errors in api controllers

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -1,7 +1,6 @@
 // Mongoose interacts with the database through its models, which is why we imported
 // the Locations model as 'wifi' at the top of the controller files
 
-
 var mongoose = require('mongoose');
 // for talking to our database
 var wifi = mongoose.model('Location');
@@ -10,8 +9,7 @@ var wifi = mongoose.model('Location');
 // arg1 is response object
 // arg2 is response code
 // arg3 is response content which is either JSON or XML, i am using JSON content here
-var sendJsonResponse = function(response,status,content) {
-
+var sendJsonResponse = function(response, status, content) {
     response.status(status);
     response.json(content);
 }
@@ -25,7 +23,31 @@ module.exports.locationsCreate = function(request, response) {
 }
 
 module.exports.locationsReadOne = function(request, response) {
-    sendJsonResponse(response, 200, {"status": "success!!!"});
+    // the parameters are held inside @param object attached to the request object
+    // access the parameter with request.params.parameter_name
+    /* With our basic controller there are three errors we need to trap
+       1.  The request parameters don't include 'locationid'
+       2.  The 'findById' method doesn't return a location
+       3.  The 'findById' method returns an error.
+    */
+    
+    if(request.params && request.params.locationid) {
+        wifi.findById(request.params.locationid)
+                .exec(function(err, location) {
+                    if(location === null) {
+                        sendJsonResponse(response, 404, {"message":"location not found"});
+                        return;
+                    } else if(err) {
+                        sendJsonResponse(response, 404, err);
+                        return;
+                    } 
+                    sendJsonResponse(response, 200, location);
+                })
+    } else {
+        sendJsonResponse(response, 404, {"message": "no locationid in request"});
+    }
+    
+    //sendJsonResponse(response, 200, {"status": "success!!!"});
 }
 
 module.exports.locationsUpdateOne = function(request, response) {
